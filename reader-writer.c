9edc9d0c665a9498095fcaa528b1b2558b6d951b
Melvin <melvinvarkey@live.com>
2014-11-07 15:27:29 +0530

    Update reader-writer.c

--- reader-writer.c
+++ reader-writer.c
@@ -1,57 +1,66 @@
-//-----------------------------------------------------
-// Reader-Writer Problem
-//-----------------------------------------------------
+//----------------------------------------------------------
+// Reader -writer problem
+//----------------------------------------------------------
 
 #include<stdio.h>
+#include<stdlib.h>
 #include<pthread.h>
 #include<semaphore.h>
 
 sem_t mutex,writeblock;
-int data = 0,rco = 0;
+int data = 0;
+int rcount = 0;
+char chdata[30]="Hello";
 
 void *reader(void *arg)
 {
-  int f;
-  f = ((int)arg);
-  sem_wait(&mutex);
-  rco = rco + 1;
-  if(rco==1)
-   sem_wait(&writeblock);
-  sem_post(&mutex);
-  printf("Data read by the reader%d is %d\n",f,data);
-  sleep(1);
-  sem_wait(&mutex);
-  rco = rco - 1;
-  if(rco==0)
-   sem_post(&writeblock);
-  sem_post(&mutex);
+      int f;
+      f = ((int)arg);
+      sem_wait(&mutex);
+      rcount = rcount + 1;
+      if(rcount==1)
+       sem_wait(&writeblock);
+      sem_post(&mutex);
+      printf("Data read by the reader %d is : ",f);
+       printf("%s\n",chdata);
+      sleep(1);
+      sem_wait(&mutex);
+      rcount = rcount - 1;
+      if(rcount==0)
+       sem_post(&writeblock);
+      sem_post(&mutex);
 }
 
 void *writer(void *arg)
 {
-  int f;
-  f = ((int) arg);
-  sem_wait(&writeblock);
-  data++;
-  printf("Data writen by the writer%d is %d\n",f,data);
-  sleep(1);
-  sem_post(&writeblock);
+      int f,i;
+      f = ((int) arg);
+      sem_wait(&writeblock);
+      data++;
+      printf("Data writen by the writer%d is \n",f+1);
+       scanf("%s",chdata);
+      sleep(1);
+      sem_post(&writeblock);
 }
 
 main()
 {
-  int i,b;  
-  pthread_t rtid[5],wtid[5];
-  sem_init(&mutex,0,1);
-  sem_init(&writeblock,0,1);
-  for(i=0;i<=2;i++)
+      int a=0,i,b; 
+      pthread_t rtid[30],wtid[30];
+      sem_init(&mutex,0,1);
+      sem_init(&writeblock,0,1);
+do
+{
+ for(i=0;i<=4;i++)
   {
-    pthread_create(&wtid[i],NULL,writer,(void *)i);
     pthread_create(&rtid[i],NULL,reader,(void *)i);
   }
-  for(i=0;i<=2;i++)
+    pthread_create(&wtid[1],NULL,writer,(void *)data);
+     pthread_join(wtid[1],NULL);
+  for(i=0;i<=4;i++)
   {
-    pthread_join(wtid[i],NULL);
     pthread_join(rtid[i],NULL);
   }
+  a++;
+}while(a!=5);
 }
